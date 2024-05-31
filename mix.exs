904731e4c7dee7dd5091a6908e840f9e21b44e8e
Qingbo Zhang <ripple0328@gmail.com>
2024-05-30 18:09:21 -0700

    fix: add dependency for test coverage

--- mix.exs
+++ mix.exs
@@ -59,7 +59,8 @@ defmodule Gatherly.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.2"},
-      {:ueberauth_google, "~> 0.12"}
+      {:ueberauth_google, "~> 0.12"},
+      {:excoveralls, "~> 0.18", only: :test}
     ]
   end
 
